5b48bfb69914cfbf640e419125e3d91c456c6d29
blackco <colin_d_black@yahoo.co.uk>
2016-09-10 13:58:03 +0100

    reorg filestructure

---
+++ web/app/scores/scores.js
@@ -0,0 +1,33 @@
+'use strict';
+
+angular.module('myApp.scores', ['ngRoute'])
+
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/scores', {
+    templateUrl: 'scores/scores.html',
+    controller: 'scoresCtrl'
+  });
+}])
+
+.controller('scoresCtrl', ['$scope', '$http',
+                          function($scope, $http) {
+	   
+	$http.get('games/get_scores.php')
+       .success(function(data,status,headers,config,statusText){
+               $scope.HttpStatus = status;
+               $scope.HttpHeaders = headers;
+               $scope.HttpConfig = config;
+               $scope.scores = data;
+               $scope.HttpStatusText = statusText;
+               $scope.foo = 'SUCCESS';
+       }).error(function(data,status,headers,config,statusText){
+               $scope.HttpStatus = status;
+               $scope.HttpHeaders = headers;
+               $scope.HttpConfig = config;
+               $scope.scores = data;
+               $scope.HttpStatusText = statusText;
+               $scope.foo = 'ERROR';
+       });
+
+
+}]);
