fbe79b2a535d8c1b8afce97207482196d99e0666
blackco <colin_d_black@yahoo.co.uk>
2016-09-15 19:27:51 +0100

    first checkin

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Deploying quiz
+
+echo 'Hello World';
+
+export FROM=/Users/blackco/Documents/java/src/quiz
+export TO=/Users/blackco/Temp2/angular-seed
+
+cp ${FROM}/web/app/view1/* ${TO}/app/view1
+cp ${FROM}/web/app/view2/* ${TO}/app/view2
+cp ${FROM}/web/app/scores/* ${TO}/app/scores
+cp ${FROM}/web/app/games/* ${TO}/app/games
+cp ${FROM}/web/app/index.html ${TO}/app
+cp ${FROM}/web/app/app.js ${TO}/app
+
+echo '${FROM} = ', ${FROM}
+
